53c8ef23140f49d3e34409544798f7357e529330
robsonsoaresid <robemsuave@gmail.com>
2026-07-05 10:49:22 -0300

    Cria renderizacao post

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
 });
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try {
+        await api.salvarPensamento({ conteudo, autoria})
+        ui.renderizarPensamentos()
+    }
+    catch {
+        alert("Error ao salvar pensamento")
+    }
+}
