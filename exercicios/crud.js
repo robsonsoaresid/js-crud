66ad5b5a148fc7b5f89cab6e3388f97c9635bf3d
robsonsoaresid <robemsuave@gmail.com>
2026-07-08 14:22:15 -0300

    Adiciona atualizacoes e cria requisicoes com delete.

---
+++ exercicios/crud.js
@@ -0,0 +1,35 @@
+// Para saber mais: CRUD em desenvolvimento de software
+
+// 1. O que é CRUD?
+// CRUD é um acrônimo para as operações básicas de manipulação de dados em sistemas de informação: Create (Criar), Read (Ler), Update (Atualizar) e Delete (Deletar). Essas operações são fundamentais para qualquer aplicação que precise gerenciar dados persistentes.
+
+// 2. Quais são as operações CRUD*?
+// Create (Criar): Responsável por criar novos registros ou recursos no sistema. Na prática, isso envolve inserir novos dados na base de dados, como adicionar um novo contato em uma lista de contatos. No caso do nosso projeto, foi a operação que fizemos de adicionar um novo pensamento no mural do Memoteca.
+
+// Read (Ler): Realiza a leitura ou recuperação de informações existentes da base de dados. É usado para consultar e visualizar dados, como listar todos os contatos salvos em um sistema. No caso do nosso projeto, foi a operação GET que fizemos para recuperar a lista de pensamentos da API fake.
+
+// Update (Atualizar): Atualiza registros existentes na base de dados com novas informações. Por exemplo, modificar o número de telefone de um contato já existente. No memoteca, o update corresponde a editar um pensamento do mural.
+
+// Delete (Deletar): Remove registros ou recursos existentes da base de dados. Isso implica na exclusão permanente dos dados, como remover um contato da lista. Como vimos nesta aula, é a operação que deleta um pensamento com o método DELETE.
+
+// 3. Qual a importância do CRUD?
+// O CRUD é amplamente utilizado para lidar com dados em diversas operações. Veja três motivos de sua importância:
+
+// Organização de dados: o CRUD define uma estrutura clara e consistente para manipular dados, garantindo que as operações sejam realizadas com segurança e eficiência;
+
+// Interação com a pessoa usuária: permite que as pessoas usuárias interajam de maneira intuitiva com a aplicação, realizando operações básicas de forma direta e compreensível;
+
+// Compatibilidade com APIs e bases de dados: a padronização das operações CRUD facilita a integração com diferentes sistemas e o uso de APIs que seguem esses princípios.
+
+// Qual a ligação entre o protocolo HTTP e métodos CRUD?
+// POST: Utilizado para criar novos recursos no servidor, correspondendo à operação de create;
+
+// GET: Usado para buscar informações ou recursos do servidor, equivalente à operação de read;
+
+// PUT: Atualiza informações de um recurso específico no servidor, correspondendo à operação de update;
+
+D// ELETE: Remove um recurso específico do servidor, equivalente à operação de delete.
+
+// São habilidades básicas de qualquer pessoa desenvolvedora a compreensão e aplicação das operações CRUD. Os conceitos e suas técnicas envolvem a manipulação de dados e seu uso pode ser generalizado e ampliado para qualquer linguagem de programação, embora cada uma implemente o CRUD de uma forma diferente.
+
+// É graças ao CRUD que podemos receber, enviar, salvar e remover informações em aplicações.
--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
+const URL_BASE = "http://localhost:3000"
+
 const api = {
   async buscarPensamentos() {
     try {
-      const response = await fetch("http://localhost:3000/pensamentos");
+      const response = await fetch(`${URL_BASE}/pensamentos`);
       return await response.json();
     } catch {
       alert("Erro ao buscar pensamentos");
@@ -11,7 +13,7 @@ const api = {
 
   async salvarPensamento(pensamento) {
     try {
-      const response = await fetch("http://localhost:3000/pensamentos", {
+      const response = await fetch(`${URL_BASE}/pensamentos`, {
         method: "POST",
         headers: {
           "Content-Type": 'application/json',
@@ -27,7 +29,7 @@ const api = {
 
   async buscarPensamentoPorId(id) {
     try {
-      const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+      const response = await fetch(`${URL_BASE}/pensamentos/${id}`);
       return await response.json();
     } catch {
       alert("Erro ao buscar pensamento");
@@ -37,7 +39,7 @@ const api = {
 
    async editarPensamento(pensamento) {
     try {
-      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+      const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
         method: "PUT",
         headers: {
           "Content-Type": 'application/json',
@@ -50,6 +52,18 @@ const api = {
       throw error;
     }
   },
+
+  async excluirPensamento(id) {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
+        method: "DELETE"
+      });
+    } catch {
+      alert("Erro ao excluir um pensamento");
+      throw error;
+    }
+  }
+
 };
 
 export default api;
