188181716326384dbee4ca9c4915bcf7e81f549c
robsonsoaresid <robemsuave@gmail.com>
2026-07-09 10:46:50 -0300

    Cria implementacao a mensagem para lista vazia

--- js/ui.js
+++ js/ui.js
@@ -16,11 +16,18 @@ const ui = {
 
   async renderizarPensamentos() {
     const listaPensamentos = document.getElementById("lista-pensamentos");
+    const mensagemVazia = document.getElementById("mensagem-vazia");
     listaPensamentos.innerHTML = ""
 
     try {
       const pensamentos = await api.buscarPensamentos();
       pensamentos.forEach(ui.adicionarPensamentoNaLista);
+      if (pensamentos.length === 0) {
+        mensagemVazia.style.display = "block";
+      } else {
+        mensagemVazia.style.display = "none";
+       
+      }
       
     } catch {
       alert("Erro ao renderizar ");
